1521dcb43799cc0f31c8c18e7e53c9434b1492ed
Riccardo Binetti <rbino@gmx.com>
2024-02-29 12:26:41 +0100

    Initial commit

---
+++ lib/spark_formatter_bug.ex
@@ -0,0 +1,18 @@
+defmodule SparkFormatterBug do
+  @moduledoc """
+  Documentation for `SparkFormatterBug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SparkFormatterBug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spark_formatter_bug_test.exs
@@ -0,0 +1,8 @@
+defmodule SparkFormatterBugTest do
+  use ExUnit.Case
+  doctest SparkFormatterBug
+
+  test "greets the world" do
+    assert SparkFormatterBug.hello() == :world
+  end
+end
